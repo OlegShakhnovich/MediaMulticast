0911b04461a36b30cbb3f65ac0c676e5717b6685
Andrei Khutsishvili <appsoftgm@gmail.com>
2025-11-21 00:40:36 +0300

    Fixed comment

--- src/udp_streaming_lib/udp_streaming_lib.cpp
+++ src/udp_streaming_lib/udp_streaming_lib.cpp
@@ -31,7 +31,7 @@ auto makeAddress(const std::string& host, const std::string& port) -> SocketAddr
     struct addrinfo* sysaddr = nullptr;
 
     std::memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;      // IPv4 или IPv6
+    hints.ai_family = AF_UNSPEC;      // IPv4 or IPv6
     hints.ai_socktype = SOCK_STREAM;  // TCP
 
     const int ERR = getaddrinfo(host.c_str(), port.c_str(), &hints, &sysaddr);
