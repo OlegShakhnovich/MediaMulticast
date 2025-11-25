73fbfca3402ef10ef7027e086a4e48a344cb7b56
Oleg Shakhnovich <oleg.schahnovich@gmail.com>
2025-11-26 00:36:40 +0100

    Revert "fix"
    This reverts commit 12973e533b317ff244527e1064a8d0d34d3afac0.

--- src/udp_streamer/main.cpp
+++ src/udp_streamer/main.cpp
@@ -1,30 +1,6 @@
-// NOLINTBEGIN(bugprone-exception-escape
-#include "file_reader/file_reader.hpp"
-
-#include <fstream>
-#include <ios>
-#include <string>
-
-#include "../udp_streaming_lib/logger/logger.hpp"
-#include "../udp_streaming_lib/logger/logger_macros.hpp"
 #include "file_reader/file_reader.hpp"
 
 auto main() -> int {
     FileReader::doSomething();
-
-    Logger::instance().setLevel(LogLevel::DEBUG);
-
-    std::ofstream ofs("app.log", std::ios::app);
-    if (ofs) {
-        Logger::instance().setSink([&ofs](std::string str) -> void {
-            ofs << str;
-            std::cout << str;
-        });
-    }
-    int MB_ZERO = 1;
-    logsys::debug("HI", " ", &MB_ZERO, " ", MB_ZERO, " ", true);
-    LOG_VAR(MB_ZERO);
     return 0;
 }
-
-// NOLINTEND
