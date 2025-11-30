1b9a4942d86208af7369a5b6cbdd990af718b540
Oleg Shakhnovich <oleg.schahnovich@gmail.com>
2025-11-30 23:58:30 +0100

    fix

--- src/udp_streaming_lib/logger/logger.hpp
+++ src/udp_streaming_lib/logger/logger.hpp
@@ -61,14 +61,14 @@ public:
         (threadOss << ... << std::forward<Args>(args));
         threadOss << '\n';
 
-        auto out = threadOss.str();
+        auto OUT = threadOss.str();
 
         Sink currentSink;
         {
             const std::scoped_lock LOCK(mutex);
             currentSink = sink;
         }
-        currentSink(std::move(out));
+        currentSink(std::move(OUT));
     }
 
     template <LogLevel Lvl, typename... Args>
